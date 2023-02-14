c7524693d4aee60690e419a1c69142371ea88f57
JD_PerezN <jd.perez998@gmail.com>
2023-02-14 15:54:59 -0500

    Update: html music file and views

--- AppTerceraPreEntrega/views.py
+++ AppTerceraPreEntrega/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from AppTerceraPreEntrega import forms, models
 
 # Create your views here.
 def inicio(req):
@@ -14,4 +17,23 @@ def pintura(req):
     return render(req, "AppTerceraPreEntrega/pintura.html")
 
 def musica(req):
-    return render(req, "AppTerceraPreEntrega/musica.html")
+
+    if req.method == "POST":
+
+        musica_form = forms.MusicaForm(req.POST)
+
+        if musica_form.is_valid():
+            musica_info = musica_form.cleaned_data
+            nueva_musica = models.Musica(
+                                        musica=musica_info["musica"],
+                                        artista=musica_info["artista"],
+                                        genero=musica_info["genero"],
+                                        lanzamiento=musica_info["lanzamiento"],
+                                        premios=musica_info["premios"])
+            nueva_musica.save()
+            return redirect("inicio")
+    
+    else:
+        musica_form = forms.MusicaForm()
+
+    return render(req, "AppTerceraPreEntrega/musica.html", {"formulario_musica": musica_form})
